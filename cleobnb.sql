871533d8b5a528e36ded19ee8cc0a0c3aab15ba4
Thaddeus Cleo <thaddeuscleo@gmail.com>
2021-08-10 21:52:46 +0700

    [Add] Script

---
+++ cleobnb.sql
@@ -0,0 +1,137 @@
+-- MariaDB dump 10.18  Distrib 10.4.17-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: cleobnb
+-- ------------------------------------------------------
+-- Server version	10.4.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `amenities`
+--
+
+DROP TABLE IF EXISTS `amenities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `amenities` (
+  `kitchen` tinyint(1) DEFAULT 0,
+  `free_parking` tinyint(1) DEFAULT 0,
+  `pets_allowed` tinyint(1) DEFAULT 0,
+  `wifi` tinyint(1) DEFAULT 0,
+  `backyard` tinyint(1) DEFAULT 0,
+  `security_camera` tinyint(1) DEFAULT 0,
+  `hot_tub` tinyint(1) DEFAULT 0,
+  `smoke_alarm` tinyint(1) DEFAULT 0,
+  `dedicated_workspace` tinyint(1) DEFAULT 0,
+  `space_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `amenities_FK` (`space_id`),
+  CONSTRAINT `amenities_FK` FOREIGN KEY (`space_id`) REFERENCES `spaces` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `space_images`
+--
+
+DROP TABLE IF EXISTS `space_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `space_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_path` varchar(100) NOT NULL,
+  `space_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `space_images_FK` (`space_id`),
+  CONSTRAINT `space_images_FK` FOREIGN KEY (`space_id`) REFERENCES `spaces` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `spaces`
+--
+
+DROP TABLE IF EXISTS `spaces`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spaces` (
+  `name` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `about` varchar(300) DEFAULT NULL,
+  `bedroom_count` int(11) NOT NULL,
+  `bed_count` int(11) NOT NULL,
+  `bathroom_count` int(11) DEFAULT NULL,
+  `shower_count` int(11) NOT NULL,
+  `bathub_count` int(11) NOT NULL,
+  `place_type` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `guess_count` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `available` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rooms_FK` (`user_id`),
+  CONSTRAINT `rooms_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_books`
+--
+
+DROP TABLE IF EXISTS `user_books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `space_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_books_FK` (`user_id`),
+  KEY `user_books_FK_1` (`space_id`),
+  CONSTRAINT `user_books_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_books_FK_1` FOREIGN KEY (`space_id`) REFERENCES `spaces` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-09 15:41:08
---
+++ scripts/bnb.php
@@ -0,0 +1,136 @@
+<?php
+
+echo "\e[91m";
+echo "  ___  __    ____  __  ____  __ _  ____ \n";
+echo " / __)(  )  (  __)/  \(  _ \(  ( \(  _ \\\n";
+echo "( (__ / (_/\\ ) _)(  O )) _ (/    / ) _ (\n";
+echo " \\___)\\____/(____)\__/(____/\\_)__)(____/\n";
+
+echo "\e[33m";
+echo "================\e[1mCLI Helper\e[0m\e[33m==============\n\n";
+echo "\e[39m";
+
+
+$username = '';
+$password = '';
+
+function createDatabase() {
+    global $username, $password;
+    $conn = new mysqli('localhost', $username, $password);
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+    echo "[INFO] Creating table\n";
+    if($conn->query("CREATE DATABASE cleobnb") === TRUE){
+        echo "[INFO] Database created successfully\n";
+    }else {
+        echo "[ERRO] Failed to create database\n";
+    }
+    $conn->close();
+}
+
+function insertTables(){
+    global $username, $password;
+    $conn = new mysqli('localhost', $username, $password, 'cleobnb');
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+    $create_query = file_get_contents("cleobnb.sql");
+    $res = $conn->multi_query($create_query);
+    if($res){
+        echo "[INFO] Tables created successfully\n";
+    }else {
+        echo "[ERRO] Failed to create tables\n";
+    }
+    $conn->close();
+}
+
+function dropAllTables(){
+    global $username, $password;
+    $conn = new mysqli('localhost', $username, $password, 'cleobnb');
+    $conn->query('SET foreign_key_checks = 0');
+    if($conn->connect_error){
+        die("[ERRO] Connection failed: ".$conn->connect_error);
+    }
+
+    if ($result = $conn->query("SHOW TABLES"))
+    {
+        while($row = $result->fetch_array(MYSQLI_NUM))
+        {
+            $conn->query('DROP TABLE IF EXISTS '.$row[0]);
+        }
+    }
+    
+    echo "[INFO] Tables drop successfully\n";
+    $conn->query('SET foreign_key_checks = 1');
+    $conn->close();
+}
+
+function dropDatabase(){
+    global $username, $password;
+    $conn = new mysqli('localhost', $username, $password);
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+    echo "[INFO] Dropping table\n";
+    if($conn->query("DROP DATABASE cleobnb") === TRUE){
+        echo "[INFO] Database dropped successfully\n";
+    }else {
+        echo "[ERRO] Failed to drop database\n";
+    }
+    $conn->close();
+}
+
+function getCreds(){
+    global $username, $password;
+    $username = readline('Input DB username (default: root): ');
+    $password = readline('Input DB password: ');
+    if(strlen($username) < 1){
+        $username = 'root';
+    }
+    echo "\e[0m";
+}
+
+
+if(isset($argv[1])){
+    if (strpos($argv[1], 'migrate') !== false) {
+        $exp = explode(':',$argv[1]);
+        if(isset($exp[1])){
+            getCreds();
+            if($exp[1] == 'full'){
+                createDatabase();
+                insertTables();
+            }else if($exp[1] == 'fresh'){
+                dropAllTables();
+                insertTables();
+            }else if($exp[1] == 'drop'){
+                dropDatabase();
+            }else {
+                echo "Usage: bnb migrate:<operation>    Example: bnb migrate:full \n";
+                echo "\e[1mmigrate:full\e[0m      Create database and insert all table\n";
+                echo "\e[1mmigrate:fresh\e[0m     Drop all table and insert all table\n";
+                echo "\e[1mmigrate:drop\e[0m      Drop database\n";
+            }
+        } else {
+            echo "Usage: bnb migrate:<operation>    Example: bnb migrate:full \n";
+            echo "\e[1mmigrate:full\e[0m      Create database and insert all table\n";
+            echo "\e[1mmigrate:fresh\e[0m     Drop all table and insert all table\n";
+            echo "\e[1mmigrate:drop\e[0m      Drop database\n";
+        }
+    } else if(strpos($argv[1], 'serve') !== false){
+        system('php -S localhost:8080');
+    } else {
+        echo "Usage: php bnb <command>\n\n";
+        echo "commands:\n";
+        echo "\e[1mmigrate:<operation>\e[0m   Database helper\n";
+        echo "\e[1mserve\e[0m                 Start local server\n";
+    }
+}else {
+    echo "Usage: php bnb <command>\n";
+    echo "commands:\n";
+    echo "\e[1mmigrate:<operation>\e[0m   Database helper\n";
+    echo "\e[1mserve\e[0m                 Start local server\n";
+}
+
+echo "\e[0m";
+?>
